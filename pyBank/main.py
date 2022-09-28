a7999e3998f51f0486caa2009c68d8b92790005f
GADH2022 <111449865+GADH2022@users.noreply.github.com>
2022-09-28 19:05:33 -0400

    count rows is done

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,9 +1,20 @@
 import os
 import csv
-csvpath= os.path.join("resources", "Resources_py_bank.csv")
+csvpath= os.path.join("pyBank\\resources\\Resources_py_bank.csv")
+# csvpath= os.path.join("C:\\Users\\appua\\OneDrive\\Desktop\\Analysis _projects\\My_Challenge1\\pyBank\\resources\\Resources_py_bank.csv")
 with open(csvpath) as csvfile :
-   csv_reader= csv.reader(csvfile,delimiter ="-")
- #  num_months = len()
-   print("Financial Analysis")
-   print ("-----------------------------------------------------")
-   print("Total months:",num_months)
+    csv_reader= csv.reader(csvfile,delimiter =",")
+    
+    #skip header
+    # csv_header=next(csv_reader) 
+    row = 0
+    for row in csv_reader:
+         #num_months = len(row)
+        print("Financial Analysis")
+        print ("-----------------------------------------------------")
+        row_count = sum(1 for row in csv_reader)
+        print(f"Total months:" ,row_count)
+        # print((row))
+    # if ((profits/losses).Value > 0)  
+           
+
