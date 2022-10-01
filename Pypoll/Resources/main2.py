852b60447e2aa7d4218c8f6d6eb5adbb1c60ed06
GADH2022 <111449865+GADH2022@users.noreply.github.com>
2022-10-01 13:26:01 -0400

    not done

---
+++ Pypoll/Resources/main2.py
@@ -0,0 +1,65 @@
+import os
+import csv
+from tkinter import N
+csvpath =os.path.join("Pypoll\\Resources\\election_data.csv")
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter = ",")
+    header =next(csvreader)
+    # skip header to count actual values
+    total_votes = 0
+    casper_votes = 0
+    diana_votes = 0
+    raymon_votes = 0
+    for row in csvreader:
+        total_votes = total_votes + 1
+        if row[2] == "Charles Casper Stockham":
+            casper_votes = casper_votes + 1
+        elif row[2] == "Diana DeGette":
+            diana_votes = diana_votes + 1
+        elif row[2] ==  "Raymon Anthony Doane":
+            raymon_votes = raymon_votes + 1   
+
+  #lists out of data 
+if((casper_votes > diana_votes) & (casper_votes > raymon_votes)):
+    winner = "Charles Casper Stockham"
+elif((diana_votes > casper_votes) & (diana_votes > raymon_votes)):
+    winner = "Diana DeGette"
+elif((raymon_votes > casper_votes) & (diana_votes < raymon_votes)):
+    winner = "Raymon Anthony Doane"
+# candidates = ["Charles casper stockham","Diana DeGette","Raymon Anthony Doane"]
+# votes = ["casper_votes","diana_votes","raymon_votes"]
+# # zip up values together as dictonary
+# dict_candidates_and_votes = dict(zip(candidates,votes))
+
+# key = max(dict_candidates_and_votes ,  key = dict_candidates_and_votes.get)
+
+casper_percent = round(((casper_votes/total_votes) * 100),3)
+diana_percent = round( ( (diana_votes/ total_votes) * 100),3)
+raymon_percent = round(( (raymon_votes/total_votes ) * 100),3)
+
+#output data 
+print("Winner :",winner)
+print("Winner :",winner)
+Analysis1 = "Election Results\n--------------------\nTotal Votes:{}\n----------------------------------------\nCharles Casper Stockham : {}% ({})\n Diana DeGette :{}% ({})\nRaymon Anthony Doane: {}% ({})\n----------------------------\nWinner:{}\n -------------- ".format(total_votes,casper_percent,casper_votes,diana_percent,diana_votes,raymon_percent,raymon_votes,winner)
+print(Analysis1)           
+print(f"Election Results")
+print("------------------------------------------------------")
+print("Total Votes:",total_votes)
+print("------------------------------------------------------")
+print(f"Charles Casper Stockham : {casper_percent:.3f}% ({casper_votes})")
+print(f"Diana DeGette : {diana_percent:.3f}% ({diana_votes})")
+print(f"Raymon Anthony Doane: {raymon_percent:.3f}% ({raymon_votes})")
+print("------------------------------------------------------------")
+print("Winner :",winner)
+print("--------------------------------------------------------------")
+
+# output to file
+
+file2= open("pypol.txt","w")
+file2.writelines(Analysis1)
+file2.close()
+
+
+
+
--- pyBank/main.py
+++ pyBank/main.py
@@ -1,20 +1,67 @@
+from cgi import print_form
 import os
 import csv
+#from tkinter.tix import COLUMN
 csvpath= os.path.join("pyBank\\resources\\Resources_py_bank.csv")
 # csvpath= os.path.join("C:\\Users\\appua\\OneDrive\\Desktop\\Analysis _projects\\My_Challenge1\\pyBank\\resources\\Resources_py_bank.csv")
 with open(csvpath) as csvfile :
     csv_reader= csv.reader(csvfile,delimiter =",")
     
-    #skip header
-    # csv_header=next(csv_reader) 
-    row = 0
+    #skip header ,Here  Header should not be skipped in counting rows
+    date = []
+    revenue = []
+    revenue_change = []
+    date_month = []
+ 
+    row_count = 0
+    total = 0
+    avg_rev_change = 0
+    date_max = {}
+    date_increase = ""
+    next(csv_reader)
+
     for row in csv_reader:
-         #num_months = len(row)
-        print("Financial Analysis")
-        print ("-----------------------------------------------------")
-        row_count = sum(1 for row in csv_reader)
-        print(f"Total months:" ,row_count)
-        # print((row))
-    # if ((profits/losses).Value > 0)  
-           
+        row_count +=  1 # we used comprehension ,to calculate number of rows
+        total = total + int(row[1])  #to count total.   
+        # revenue.append(int(row[1]))
+        revenue.append(int(row[1]))
+        date.append(row[0])
+    print(revenue)
+    print(date)
+#calculate changes in profits/losses over entire period,and average of those changes
+    
+    for i in range(1,len(revenue)):   
+           revenue_change.append(revenue[i] - revenue[i-1])
+           avg_rev_change = round(sum(revenue_change)/len(revenue_change),2)
+
+    date_max=dict(zip(revenue_change, date))
+    print(date_max) 
+    max_increase_value = max(revenue_change)
+    min_increase_value = min(revenue_change)   
+    for key,value in date_max.items():
+        if(date_max[key]-1 == max_increase_value):
+            date_increase = value
+        #   date_increase_1 = value+1 
+    # for max_increase_value in date_max.items():
+    #     date_increase = {date, max_increase_value}
+    print(date_increase)
+    # print(date_increase+1)
+    # max_increase_month = revenue_change.index(max_increase_value)+ 1
+    # max_decrease_month = revenue_change.index(min_increase_value)+ 1 
+    # print(max_increase_month) 
+    # print(max_decrease_month)  
+    print("Financial Analysis")
+    print("-------------------------------------------------------") 
+    print("total months:" ,row_count)
+    print("Total:" , "$" , total)
+    print("Average Change: ","$" , avg_rev_change)
+
+    # print(f"Greatest Increase in Profits:", {max_increase_month}, "$" ,(int(max_increase_value)))
+    # print(f"Greatest Decrease in Profits:", {max_decrease_month}, "$" ,(int(min_increase_value)))
+    
+    #calculate Greatest increase in profits(date and amount)
+    #calculate Greatest decrease in profits(date and amount)
+#output to file
+#file1 = open("pybank.txt","w") 
+#file1.writelines() #Write analysis into pybank.txt
 
